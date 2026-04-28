5896049811652ce095a67a59948e914b26186ab4
Carlos Eduardo Rodrigues dos Santos <carlos.rodrigues@recife.pe.gov.br>
2026-04-28 10:17:06 -0300

    Finaliza CRUD em memória com rotas de atualização (PUT) e exclusão (DELETE)

--- car_api/routers/users.py
+++ car_api/routers/users.py
@@ -16,7 +16,7 @@ router = APIRouter()
 )
 async def create_user(user: UserSchema):
     user_with_id = UserPublicSchema(
-        **user.model_dump(),
+        **user.model_dump(), # para desestruturar o usuário user (atributos UserSchema)
         id=len(USERS) + 1,
     )
     USERS.append(user_with_id)
@@ -48,3 +48,21 @@ async def list_users():
         # ]
     }
 
+@router.put(
+        path='/{user_id}',
+        status_code=status.HTTP_201_CREATED,
+        response_model=UserPublicSchema,
+)
+async def update_user(user_id: int, user: UserSchema):
+    user_with_id = UserPublicSchema(**user.model_dump(), id=user_id)
+    USERS[user_id -1] = user_with_id
+    return user_with_id
+
+
+@router.delete(
+        path='/{user_id}',
+        status_code=status.HTTP_204_NO_CONTENT,
+)
+async def delete_user(user_id: int):
+    del USERS[user_id -1]
+    return
